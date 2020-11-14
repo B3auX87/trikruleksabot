e975f892f59d1bf6ee5c3948394ad5c72b93dcbf
B3auX87 <devtrikru@gmail.com>
2020-11-14 21:56:38 +0100

    weather cmd added

---
+++ commands/misc/weather.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const weather = require("weather-js");
+
+module.exports = {
+    commands: 'weather',
+    expectedArgs: '<City>',
+    minArgs: 1,
+    maxArgs: 1,
+    callback: (message, args) => {
+
+        let city = args.join(" ");
+        let degreetype = "C"; // You can change it to F. (fahrenheit.)
+
+        await weather.find({ search: city, degreeType: degreetype }, function (err, result) {
+
+            if (err || result === undefined || result.length === 0) return message.channel.send("Unbekannte Stadt ...");
+
+            let current = result[0].current;
+            let location = result[0].location;
+
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(current.observationpoint)
+                .setDescription(`> ${current.skytext}`)
+                .setThumbnail(current.imageUrl)
+                .setColor(0x7289DA)
+                .addFields(
+
+                    {
+                        name: '> Breitengrad',
+                        value: location.lat,
+                    },
+                    {
+                        name: '> Längengrad',
+                        value: location.long,
+                    },
+                    {
+                        name: '> gefühlt wie',
+                        value: `${current.feelslike}°`,
+                    },
+                    {
+                        name: '> Einheit Typ',
+                        value: location.degreetype,
+                    },
+                    {
+                        name: '> Wind',
+                        value: current.winddisplay,
+                    },
+                    {
+                        name: '> Luftfeuchtigkeit',
+                        value: `${current.humidity} %`,
+                    },
+                    {
+                        name: '> Timezone',
+                        value: `GMT ${location.timezone}`,
+                    },
+                    {
+                        name: '> Temperatur',
+                        value: `${current.temperature}°`,
+                    },
+                    {
+                        name: '> Beobachtungs Zeit',
+                        value: current.observationtime,
+                    })
+
+            message.channel.send(embed)
+        })
+    }
+}
