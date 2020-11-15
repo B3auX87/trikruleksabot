a5bbc2e7561b03f4dcc178f82adeb072645f1f82
B3auX87 <devtrikru@gmail.com>
2020-11-15 13:13:57 +0100

    Design Update

--- commands/misc/ahelp.js
+++ commands/misc/ahelp.js
@@ -17,16 +17,41 @@ module.exports = {
             .setTitle('【A】【H】【E】【L】【P】')
             .setThumbnail('https://media.giphy.com/media/phJ6eMRFYI6CQ/giphy.gif')
             .setDescription(`  **Ｓ𝐞яν𝒆𝐫**  :   ** ${message.member.guild.name} **
-            ˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜
-            **${prefix}cc oder [${prefix}purge]** -- Lösche die Nachrichten (Pins ausgenommen!)
-            **${prefix}status <Text>** -- Setze die Activity vom Bot
-            **${prefix}ban <@Name>** -- Member Bannen
-            **${prefix}kick <@Name>** -- Member kicken
-            **${prefix}eval <Text>** -- Mathe Helper (Owner Zugang)
-            **${prefix}invites** -- Zeigt an wer wieviel eingaladen hat
-            **${prefix}clticket** -- Schließe ein Ticket
-            **${prefix}cembed <titel> <Text> <#HexCode>** -- Erstelle ein Custom Embed
-            `)
+            ˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜`)
+            .addFields(
+                {
+                    name: `✻ **${prefix}cc oder [${prefix}purge]**`,
+                    value: '》 Lösche die Nachrichten (Pins ausgenommen!)',
+                },
+                {
+                    name: `✻ **${prefix}status <Text>**`,
+                    value: '》 Setze die Activity vom Bot',
+                },
+                {
+                    name: `✻ **${prefix}ban <@Name>**`,
+                    value: '》 Member Bannen',
+                },
+                {
+                    name: `✻ **${prefix}kick <@Name>**`,
+                    value: '》 Member kicken',
+                },
+                {
+                    name: `✻ **${prefix}eval <numm1 + / - / * / / num2>**`,
+                    value: '》 Mathe Helper (Owner Zugang)',
+                },
+                {
+                    name: `✻ **${prefix}invites**`,
+                    value: '》 Zeigt an wer wieviel eingaladen hat',
+                },
+                {
+                    name: `✻ **${prefix}clticket**`,
+                    value: '》 Schließe ein Ticket',
+                },
+                {
+                    name: `✻ **${prefix}cembed <titel> <Text> <#HexCode>**`,
+                    value: '》 Erstelle ein Custom Embed',
+                },
+            )
             .setTimestamp(message.createdTimestamp)
             .setFooter('mc.trikru.de', 'https://wheedesign.com/img/design/13459094.png');
 
