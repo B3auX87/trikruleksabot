7777a74dc68d921bbcca3a45572e0ab3d08d2706
B3auX87 <devtrikru@gmail.com>
2020-11-15 12:34:33 +0100

    help designed

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -15,22 +15,53 @@ module.exports = {
             .setTitle('【H】【E】【L】【P】')
             .setThumbnail('https://media.giphy.com/media/AXOdttXiL8MeI/giphy.gif')
             .setDescription(`  **Ｓ𝐞яν𝒆𝐫**  :   ** ${message.member.guild.name} **
-            ˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜
-
-            **${prefix}ahelp** -- Admin Befehle
-            **${prefix}info** -- Zeigt Server Infos an
-            **${prefix}ping** -- Zeigt deinen und den Bots Ping an
-            **${prefix}add <num1> <num2>** -- rechnet 2 zahlen zusammen
-            **${prefix}cticket/createticket** -- eröffne ein Ticket
-            **${prefix}meme/memes** -- Zeigt Random Memes an
-            **${prefix}wetter/weather <Stadt/Postleitzahl>** -- Wetter infos
-            **${prefix}ascii <Text>** -- cooler Text style
-            **${prefix}spotify [<@name>] -- Spotify Informationen
-            **${prefix}suggestion <Text>** -- mache einen Vorschlag im Suggestion Channel\n
-            **${prefix}cases <Land>** -- covid Status Verfügbare Länder:
-            germany, poland, united-kingdom, italy, united-states, denmark, russia, france, austria (österreich), luxembourg, switzerland,
-            czech-republic, netherlands, belgium\n
-            `)
+            ˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜˜”*°••°*”˜`)
+            .addFields(
+                {
+                    name: `⑉ **${prefix}ahelp**`,
+                    value: '》 Admin Befehle',
+                },
+                {
+                    name: `⑉ **${prefix}info**`,
+                    value: '》 Zeigt Server Infos an',
+                },
+                {
+                    name: `⑉ **${prefix}ping**`,
+                    value: '》 Zeigt deinen und den Bots Ping an',
+                },
+                {
+                    name: `⑉ **${prefix}add <num1> <num2>**`,
+                    value: '》 rechnet 2 zahlen zusammen',
+                },
+                {
+                    name: `⑉ **${prefix}cticket/createticket**`,
+                    value: '》 eröffne ein Ticket',
+                },
+                {
+                    name: `⑉ **${prefix}meme/memes**`,
+                    value: '》 Zeigt Random Memes an',
+                },
+                {
+                    name: `⑉ **${prefix}wetter/weather <Stadt/Postleitzahl>**`,
+                    value: '》 Wetter infos',
+                },
+                {
+                    name: `⑉ **${prefix}ascii <Text>**`,
+                    value: '》 Style dein text',
+                },
+                {
+                    name: `⑉ **${prefix}spotify [<@name>]**`,
+                    value: '》 Spotify Informationen',
+                },
+                {
+                    name: `⑉ **${prefix}suggestion <Text>**`,
+                    value: '》 mache einen Vorschlag im Suggestion Channel',
+                },
+                {
+                    name: `⑉ **${prefix}cases/covid <Land>**`,
+                    value: '》 covid Status Verfügbare Länder: germany, poland, united-kingdom, italy, united-states, denmark, russia, france, austria (österreich), luxembourg, switzerland, czech-republic, netherlands, belgium',
+                },
+            )
             .setTimestamp(message.createdTimestamp)
             .setFooter('mc.trikru.de', 'https://wheedesign.com/img/design/13459094.png');
 
